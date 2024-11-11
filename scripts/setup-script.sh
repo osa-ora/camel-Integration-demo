5e72e250ec43393ea53f62b99b2e3c174c21a1cc
Osama Oransa <osa_ora@yahoo.com>
2024-11-11 09:04:54 +0200

    Update setup-script.sh

--- scripts/setup-script.sh
+++ scripts/setup-script.sh
@@ -8,6 +8,7 @@ echo "Please Login to OCP using oc login ..... "
 echo "Make sure Openshift AMQ Broker Operator is installed"
 echo "Make sure Openshift AMQ Streams Operator is installed"
 echo "Make sure oc command is available"
+read
 
 # Create project
 oc new-project $1
@@ -15,8 +16,7 @@ oc new-project $1
 oc new-app mysql-persistent -p DATABASE_SERVICE_NAME=mysql -p MYSQL_USER=test -p MYSQL_PASSWORD=test123 -p MYSQL_DATABASE=accountdb
 
 COMMAND="create table accountdb.account ( id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, firstname VARCHAR( 255 ) NOT NULL, lastname VARCHAR( 255 ) NOT NULL,status INT NOT NULL);insert into accountdb.account (id, firstname, lastname, status) values (1,'Osama','Oransa',1);insert into accountdb.account (id, firstname, lastname, status) values (2,'Osa','Ora',1);"
-# Display Schema and data installing sql statements
-echo "Will install: $COMMAND"
+
 echo "Press [Enter] key to setup the DB once MySQL pod started successfully ..." 
 read
 
@@ -25,16 +25,18 @@ POD_NAME=$(oc get pods -l=name=mysql -o custom-columns=POD:.metadata.name --no-h
 echo "MySQL Pod name $POD_NAME"
 
 # Install the DB schema and add some data
+# Display Schema and data installing sql statements
+echo "Will install: $COMMAND"
 oc exec $POD_NAME -- mysql -u root accountdb -e "create table accountdb.account ( id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, firstname VARCHAR( 255 ) NOT NULL, lastname VARCHAR( 255 ) NOT NULL,status INT NOT NULL);insert into accountdb.account (id, firstname, lastname, status) values (1,'Osama','Oransa',1);insert into accountdb.account (id, firstname, lastname, status) values (2,'Osa','Ora',1);"  
 
 # Download database properties
-# TODO fix the URL
 curl https://raw.githubusercontent.com/osa-ora/camel-Integration-demo/scripts/datasource.properties >datasource.properties
 
 # Create DB secret
 oc create secret generic my-datasource --from-file=datasource.properties
 
-
+echo "Press [Enter] key to setup the Kafka cluster ..." 
+read
 # Provision Kafka using object details
 oc apply -f https://raw.githubusercontent.com/osa-ora/camel-Integration-demo/scripts/kafka-sample/kafka-topic.yaml
 
@@ -44,6 +46,8 @@ curl https://raw.githubusercontent.com/osa-ora/camel-Integration-demo/scripts/ka
 # Create secret
 oc create secret generic my-kafka-props --from-file=kafka-config.properties
 
+echo "Press [Enter] key to setup the AMQ broker cluster ..." 
+read
 # Provision AMQ using object details
 oc apply -f https://raw.githubusercontent.com/osa-ora/camel-Integration-demo/scripts/amq-broker.yaml
 
@@ -55,6 +59,8 @@ curl https://raw.githubusercontent.com/osa-ora/camel-Integration-demo/scripts/am
 # Create secret
 oc create secret generic my-amq-config --from-file=amq-config.properties
 
+echo "Press [Enter] key to deploy Camel App ..." 
+read
 # Deploy SpringBoot Application
 oc new-app --name=camel-app java~https://github.com/osa-ora/camel-Integration-demo --context-dir=camel-app -n $1
 oc expose svc/camel-demo -n $1
