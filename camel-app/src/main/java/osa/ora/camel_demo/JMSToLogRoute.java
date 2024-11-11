4372fc879db6283c5a4e3d60d73fb5abcb8cd11e
Osama Oransa <osa_ora@yahoo.com>
2024-11-11 12:21:59 +0200

    Update JMSToLogRoute.java

--- camel-app/src/main/java/osa/ora/camel_demo/JMSToLogRoute.java
+++ camel-app/src/main/java/osa/ora/camel_demo/JMSToLogRoute.java
@@ -9,7 +9,7 @@ public class JMSToLogRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         // Use Spring Boot's application properties for dynamic JMS configuration
-        from("amqp:transfers?brokerUrl={{amqp.brokerUrl}}")
+        from("amqp:transfers")
             .log("Recieved JMS message by the listener: ${body}");
     }
 }
